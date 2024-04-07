95eb062ae428e70011df619ec4e8dd5372f3d345
rooreckless <rooreckless@gmail.com>
2024-04-07 21:54:45 +0900

    actions test and Modify AboutPage

--- front_v_tail/src/components/About.vue
+++ front_v_tail/src/components/About.vue
@@ -17,7 +17,7 @@
 
   <p class="font-udev">と進んでいきます。</p>
 
-  <p class="font-udev">第22回では、これらの総まとめとして、EC2インスタンスにデプロイし<a href="http://www.roosmemosite.net/">http://www.roosmemosite.net/</a>に公開するようにしています。<br>(↑実験が終了したため、現在は非公開かもしれません。)</p>
+  <p class="font-udev">第22回では、これらの総まとめとして、EC2インスタンスにデプロイしhttp://www.roosmemosite.net/に公開するようにしています。<br>(↑実験が終了にともないこちらは閉鎖しています。)</p>
 
   <p class="font-udev">各記事は非常に記述量が多いです。<br>(参考サイトや考え方だけでなく、実装コードとその変遷、実行結果や挙動確認の様子も画像やGifアニメ画像で掲載しているため)</p>
 
@@ -29,7 +29,7 @@
   
   <hr class="border-2">
 
-  <p class="font-udev">追記 : 2024/4/7現在ではサイトをリニューアルし、<a class="text-sky-500 hover:text-red-500" href="https://www.roosmdblogsite.net/">https://www.roosmdblogsite.net/</a>になります。</p>
+  <p class="font-udev">追記 : 2024/4/7現在では当サイトをリニューアルし、<a class="text-sky-500 hover:text-red-500" href="https://www.roosmdblogsite.net/">https://www.roosmdblogsite.net/</a>になります。</p>
 
   <p class="font-udev">(Githubリポジトリは<a href="https://github.com/rooreckless/mdblog2_backend" class="text-sky-500 hover:text-red-500">https://github.com/rooreckless/mdblog2_backend</a><br>
   と<br>
